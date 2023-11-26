9fb75f80b62dfd9354c151a3f35c9a85830bb06e
qpzr <58745620+qpzr@users.noreply.github.com>
2023-11-27 03:39:51 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -4,10 +4,10 @@ echo '修改密码'
 sed -i 's/#define\s*DEF_ROOT_PASSWORD\s*"admin"/#define  DEF_ROOT_PASSWORD     "123"/g' ./user/shared/defaults.h
 
 echo '修改NTP1为国家授时中心'
-sed -i "s/ntp1.aliyun.com/ntp.ntsc.ac.cn/g" ./user/shared/defaults.h
+sed -i "s/ntp1.aliyun.com/cn.ntp.org.cn/g" ./user/shared/defaults.h
 
 echo '修改NTP2为中国NTP快速授时服务'
-sed -i "s/2001:470:0:50::2/cn.ntp.org.cn/g" ./user/shared/defaults.h
+sed -i "s/2001:470:0:50::2/ntp.ntsc.ac.cn/g" ./user/shared/defaults.h
 
 echo '超级精简配置'
 sed -i '23,63d' ./build_firmware_modify
