174ee3389cde3a061506882ef5b36d37a69d5984
qpzr <58745620+qpzr@users.noreply.github.com>
2023-12-26 09:04:42 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -7,7 +7,8 @@ echo '修改NTP1为国家授时中心'
 sed -i "s/ntp1.aliyun.com/cn.ntp.org.cn/g" ./user/shared/defaults.h
 
 echo '修改NTP2为中国NTP快速授时服务'
-sed -i "s/2001:470:0:50::2/ntp.ntsc.ac.cn/g" ./user/shared/defaults.h
+#sed -i "s/2001:470:0:50::2/ntp.ntsc.ac.cn/g" ./user/shared/defaults.h
+sed -i "s/time1.cloud.tencent.com/ntp.ntsc.ac.cn/g" ./user/shared/defaults.h
 
 echo '超级精简配置'
 sed -i 's/#CONFIG_FIRMWARE_CPU_SLEEP/CONFIG_FIRMWARE_INCLUDE_LANG_CN/g' ./configs/templates/*.config
